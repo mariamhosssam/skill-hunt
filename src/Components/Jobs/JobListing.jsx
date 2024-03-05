81b1572560878585d8fe84fb47e8598bbe14de38
shrouk <shroukilany58@gmail.com>
2024-03-06 00:06:28 +0200

    Rabt el api

--- src/Components/Jobs/JobListing.jsx
+++ src/Components/Jobs/JobListing.jsx
@@ -4,20 +4,20 @@ import jobs from "./jobs"
 
 const JobListing = props => {
 
-    useEffect(() => {
-        // here we will receive jobs from backend instead of importing them
-        console.log('jobs:', props.jobs)
-    },[])
-    return (
-        <section className="site-section">
-        <div className="container">
-          <div className="row mb-5 justify-content-center">
-            <div className="col-md-7 text-center">
-              <h2 className="section-title mb-2">{jobs.jobsCount} Job Listed</h2>
-            </div>
-          </div>
-          <ul className="job-listings mb-5">
-            {props.jobs.jobs.map(j => <Job img={j.image} title={j.title} company={j.company} type={j.type}></Job>)}
+    // useEffect(() => {
+    //     // here we will receive jobs from backend instead of importing them
+    //     console.log('jobs:', props.jobs)
+    // },[])
+    // return (
+    //     <section className="site-section">
+    //     <div className="container">
+    //       <div className="row mb-5 justify-content-center">
+    //         <div className="col-md-7 text-center">
+    //           <h2 className="section-title mb-2">{jobs.jobsCount} Job Listed</h2>
+    //         </div>
+    //       </div>
+    //       <ul className="job-listings mb-5">
+    //         {props.jobs.jobs.map(j => <Job img={j.image} title={j.title} company={j.company} type={j.type}></Job>)}
             {/* <li className="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
               <a href="job-single.html" />
               <div className="job-listing-logo">
@@ -37,7 +37,7 @@ const JobListing = props => {
                 </div>
               </div>
             </li> */}
-          </ul>
+        //  </ul>
           <div className="row pagination-wrap">
             <div className="col-md-6 text-center text-md-left mb-4 mb-md-0">
               <span>Showing 1-7 Of {jobs.jobsCount} Jobs</span>
@@ -61,9 +61,9 @@ const JobListing = props => {
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    )
+       // </div>
+    //  </section>
+    //)
 }
 
 export default JobListing
