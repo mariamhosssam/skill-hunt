1a725310eb9ee4693f158ea5e3da5562acdef763
OsamaElkorashy <46675528+OsamaElkorashy@users.noreply.github.com>
2024-05-04 02:46:30 +0300

    using query params for job id in techincalq

--- src/Pages/TechnicalQ.jsx
+++ src/Pages/TechnicalQ.jsx
@@ -3,24 +3,31 @@ import Footer from "../Components/Footer"
 import Header from "../Components/Header"
 import { useState, useEffect } from 'react';
 import { baseUrl } from "../Helpers";
+import { useSearchParams } from "react-router-dom";
 
 const TechnicalQ = () => {
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
   const [responseMessage, setResponseMessage] = useState({});
+  const [searchParams] = useSearchParams();
+  const [loadingErrorMessage, setLoadingErrorMessage] = useState('')
   // const [SessionId, setSessionId] = useState('');
   // const [data, setData] = useState(null);
-  const jobId = 1;
+  let jobId;
   const token = localStorage.getItem('token')
   let sessionId
 
 
   useEffect(() => {
+    jobId = searchParams.get('jobId')
     const CreateSession = async () => {
       axios.post(`${baseUrl}/TechnicalInterview/CreateSession?token=${token}&jobId=${jobId}`)
         .then(response => {
           sessionId = response.data;
-        }).catch(err => console.error(err));
+        }).catch(err => (
+          console.error(err),
+          setLoadingErrorMessage(err.response.data)
+        ));
     };
     const GetTechnicalQuestions = async () => {
       axios.get(`${baseUrl}/TechnicalInterview/GetTechnicalQuestions?token=${token}&jobId=${jobId}`)
@@ -53,22 +60,22 @@ const TechnicalQ = () => {
   //   .catch(e => console.log(e) )
   // }
   const handleSubmit = async () => {
-      axios.post(`${baseUrl}/TechnicalInterview/SubmitTheAnswers?token=${token}&jobId=${jobId}`, answers)
-      .then(response => setResponseMessage({ message: response.data, state: 'success'}))
+    axios.post(`${baseUrl}/TechnicalInterview/SubmitTheAnswers?token=${token}&jobId=${jobId}`, answers)
+      .then(response => setResponseMessage({ message: response.data, state: 'success' }))
       .catch(err => (
-        setResponseMessage({ message: 'An error occurred, please try again!', state: 'error'}),
+        setResponseMessage({ message: 'An error occurred, please try again!', state: 'error' }),
         console.log(err)
       )
-    )
+      )
   };
   const handleAnswer = (event, id) => {
     console.log(id, event.target.value)
     const answer = answers.find(a => a.questionId == id)
-    if(answer) {
+    if (answer) {
       answer.userAnswer = event.target.value
       setAnswers(answers)
     } else {
-      setAnswers(prev => [...prev, {questionId: id.toString(), userAnswer: event.target.value}])
+      setAnswers(prev => [...prev, { questionId: id.toString(), userAnswer: event.target.value }])
     }
     setTimeout(() => {
       console.log(answers)
@@ -82,43 +89,46 @@ const TechnicalQ = () => {
       <section className="site-section">
         <div className="container">
           <div className="quiz-container">
-            {questions?.map((question, i) => (
+            {loadingErrorMessage && <h3 id="results" className="text-danger">{loadingErrorMessage}</h3>}
+            {!loadingErrorMessage && <>
+              {questions?.map((question, i) => (
 
-              <div className="row form-group" key={question.id}>
-                <div className="col-md-12 mb-3 mb-md-0">
-                  <label className="text-black" htmlFor={question.id}>
-                    {question.questionBody}
-                  </label>
-                  {question.answers.length > 0 ?
-                    (question.answers.map(answer => (
-                      <div key={answer.id}>
-                        <input onChange={(e) => handleAnswer(e, question.id)} type="radio" id={answer.id} name={question.id} value={answer.chooseBody} />{'  '}
-                        <label htmlFor={answer.chooseBody}>{answer.chooseBody}</label>
-                      </div>
-                    ))
-                    )
-                    : (<input
-                      onBlur={(e) => handleAnswer(e, question.id)}
-                      type="text"
-                      id={question.id}
-                      // value={username}
-                      className="form-control"
-                    // placeholder="Username"
-                    />)
-                  }
+                <div className="row form-group" key={question.id}>
+                  <div className="col-md-12 mb-3 mb-md-0">
+                    <label className="text-black" htmlFor={question.id}>
+                      {question.questionBody}
+                    </label>
+                    {question.answers.length > 0 ?
+                      (question.answers.map(answer => (
+                        <div key={answer.id}>
+                          <input onChange={(e) => handleAnswer(e, question.id)} type="radio" id={answer.id} name={question.id} value={answer.chooseBody} />{'  '}
+                          <label htmlFor={answer.chooseBody}>{answer.chooseBody}</label>
+                        </div>
+                      ))
+                      )
+                      : (<input
+                        onBlur={(e) => handleAnswer(e, question.id)}
+                        type="text"
+                        id={question.id}
+                        // value={username}
+                        className="form-control"
+                      // placeholder="Username"
+                      />)
+                    }
+                  </div>
                 </div>
-              </div>
 
-              // <div id={"quiz-"+i} key={question.id}>
-              //   <div>{question.questionBody}</div>
-              //   {question.answers.length > 0 ? 
-              //   question.answers.map(answer => <div><input type="radio" id={answer.id} name={question.id} value={answer.chooseBody}/> <label for={answer.chooseBody}>{answer.chooseBody}</label></div> ) 
-              //   : <input type="text"></input>}
+                // <div id={"quiz-"+i} key={question.id}>
+                //   <div>{question.questionBody}</div>
+                //   {question.answers.length > 0 ? 
+                //   question.answers.map(answer => <div><input type="radio" id={answer.id} name={question.id} value={answer.chooseBody}/> <label for={answer.chooseBody}>{answer.chooseBody}</label></div> ) 
+                //   : <input type="text"></input>}
 
-              // </div>
+                // </div>
 
-            ))}
-            <button id="submit-btn" className="btn px-4 btn-primary text-white" onClick={handleSubmit}>Submit Answers</button>
+              ))}
+              <button id="submit-btn" className="btn px-4 btn-primary text-white" onClick={handleSubmit}>Submit Answers</button>
+            </>}
           </div>
           <br></br>
           <div id="results" className={responseMessage.state === 'success' ? "text-success" : "text-danger"}>{responseMessage.message}</div>
