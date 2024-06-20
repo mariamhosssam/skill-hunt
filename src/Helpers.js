587c1a18d522df83a4ab2034789b580f5140a0ea
Amin Elsayed <aminelsayed202020@gmail.com>
2024-06-20 23:38:47 +0300

    just test

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://localhost:44322/api'
-// export const baseUrl = 'https://localhost:7176/api'
+//export const baseUrl = 'https://localhost:44322/api'
+ export const baseUrl = 'https://localhost:7176/api'
